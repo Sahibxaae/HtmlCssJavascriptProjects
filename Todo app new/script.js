236e28ed8bfe1d23172ea6ad7d23ce58ae169a55
Sahibxaae <mohamedsahib.xaae@gmail.com>
2024-12-19 18:21:33 +0530

    made changes in label element gave same id value to for attribut in label in script.js

--- Todo app new/script.js
+++ Todo app new/script.js
@@ -9,10 +9,10 @@ addBtn.addEventListener('click',(event)=>{
     if(addTask.value.length != ""){
         list.push(addTask.value);
         console.log(list);
-    ul.innerHTML +=`<li id="${list.length-1}">
+    ul.innerHTML +=`<li id="${list.length-1}" class="list">
           <input type="checkbox" id="todo-1" >
-                    <label for="todo-1" class="custom-checkbox"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="transparent"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></label>
-                    <label for="todo-1" class="todo-text">${addTask.value}</label>
+                    <label for="${list.length}" class="custom-checkbox"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="transparent"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></label>
+                    <label for="${list.length}" class="todo-text">${addTask.value}</label>
                     <button class="delete-button" onclick="complete(${list.length-1})"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--secondary-color)"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
         </li></li>`;
     addTask.value="";
